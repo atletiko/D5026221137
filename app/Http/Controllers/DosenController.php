4f36ae466f3400358ac9e410e3c39a67f722f3f5
atletiko <atletiko@gmail.com>
2023-11-13 15:32:09 +0700

    first commit

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DosenController extends Controller
+{
+    public function index(){
+        $a = 3;
+        $b = 7;
+        $c = 7 * 3;
+    	return "<h1>Hasil perkalian = " . (string)$c . "</h1>";
+    }
+
+    public function showBlog(){
+        $nama = "Felicia";
+        $alamat = "Keputih, Surabaya";
+        $umur = 20;
+        return view('blog', ['nama' => $nama, 'alamat' => $alamat, 'umur' => $umur]);
+    }
+
+    public function shownama($nama){ // $nama itu sama dengan {nama} di web.php
+        return "Anda telah mengisikan : " . $nama;
+    }
+
+    public function formulir(){
+        //cek soal hak akses
+    	return view('formulir');
+
+}
+
+public function proses(Request $request){
+    $nama = $request->input('nama');
+     $alamat = $request->input('alamat');
+    return "Nama : ".$nama.", Alamat : ".$alamat;
+}
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,85 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+    return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+
+Route::get('halo2', function () {
+    return "<h1>Halo, Selamat datang</h1>";
+});
+
+Route::get('/blog2', function () {
+    return view('blog');
+});
+
+Route::get('/5026221072', function () {
+    return view('5026221072');
+});
+
+Route::get('/alerts', function () {
+    return view('alerts');
+});
+
+Route::get('/hello', function () {
+    return view('hello');
+});
+
+Route::get('/js1', function () {
+    return view('js1');
+});
+
+Route::get('/js2', function () {
+    return view('js2');
+});
+
+Route::get('/latihan1', function () {
+    return view('latihan1');
+});
+
+Route::get('/link', function () {
+    return view('link');
+});
+
+Route::get('/style', function () {
+    return view('style');
+});
+
+Route::get('/validasi1', function () {
+    return view('validasi1');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
+Route::get('show', 'App\Http\Controllers\DosenController@showBlog');
+
+Route::get('/pegawai/{nama}', 'App\Http\Controllers\DosenController@shownama');
+
+Route::get('/formulir', 'App\Http\Controllers\DosenController@formulir');
+Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
+
+// route blog
+Route::get('/blog', 'App\Http\Controllers\BlogController@home');
+Route::get('/blog/tentang', 'App\Http\Controllers\BlogController@tentang');
+Route::get('/blog/kontak', 'App\Http\Controllers\BlogController@kontak');
+
+
